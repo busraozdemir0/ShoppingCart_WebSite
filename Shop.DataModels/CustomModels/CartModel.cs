0719b163a5d76af2f8095cbddc8104300cf8597e
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-05-23 16:57:19 +0300

    Sepet sayfası yapıldı (sepete eklenen urunler ve toplam fiyat) - 9

--- Shop.DataModels/CustomModels/CartModel.cs
+++ Shop.DataModels/CustomModels/CartModel.cs
@@ -15,7 +15,7 @@ namespace Shop.DataModels.CustomModels
         public int Quantity { get; set; }
         public int Price { get; set; }
         public int AvailableStock { get; set; }
-        public int ShippingCharges { get; set; }
+        public int ShippingCharges { get; set; } // Kargo ucreti
         public int SubTotal { get; set; }
         public int Total { get; set; }
         public string ShippingAddress { get; set; }
